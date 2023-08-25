5b5dc71d64f90c95906c27f06d3c0bc139350d7c
temple7447 <starukido@gmail.com>
2023-08-25 03:30:23 +0100

    first commit

---
+++ src/Store.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+const Store = () => {
+  return (
+    <div>
+    <Link to='/'>Store</Link>
+      
+    </div>
+  )
+}
+
+export default Store
